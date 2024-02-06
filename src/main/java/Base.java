6ae99502073072d2f6c376b9d1d1732b938a5cab
Esteban Labastie <elabastie@iutbayonne.univ-pau.fr>
2024-02-06 14:55:26 +0100

    Pousser vers le bas

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,12 +1,13 @@
 public class Base {
 
-    protected int doubleValue(int number) {
-        return number + number;
-    }
 }
 class Derived extends Base {
     public static void main(String[] args) {
         Derived subject = new Derived();
         System.out.println("Doubling 21. Result: " + subject.doubleValue(21));
+    }
+
+    protected int doubleValue(int number) {
+        return number + number;
     }
 }
